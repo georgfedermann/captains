57441f690d84bc7a6802871e04620c0fb0292827
Georg Federmann <georg.federmann@gmail.com>
2020-04-25 14:11:48 +0200

    add some unit tests

--- src/test/java/com/sneaky/data/persistence/dao/JpaOfficerDaoTest.java
+++ src/test/java/com/sneaky/data/persistence/dao/JpaOfficerDaoTest.java
@@ -1,6 +1,7 @@
 package com.sneaky.data.persistence.dao;
 
 import com.sneaky.data.persistence.entities.Officer;
+import com.sneaky.data.persistence.entities.Rank;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,9 +9,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest
 @Transactional
@@ -22,6 +27,53 @@ class JpaOfficerDaoTest {
 
     @Autowired
     private JdbcTemplate template;
+
+    @Test
+    public void saveTest() throws Exception {
+        Officer officer = new Officer(Rank.LIEUTENANT, "Nyota", "Uhura");
+        assertThat(officer.getId()).isNull();
+        dao.save(officer);
+        assertAll(
+                () -> assertThat(officer.getId()).isEqualTo(6)
+        );
+    }
+
+    @Test
+    public void findByIdThatExists() throws Exception {
+        IntStream.rangeClosed(1, 5).forEach(
+                id -> {
+                    System.out.println(String.format("Freaking id is %d.", id));
+                    Optional<Officer> officer = dao.findById(id);
+                    assertAll(
+                            () -> assertThat(officer.isPresent()),
+                            () -> assertThat(officer.get().getId()).isEqualTo(id)
+                    );
+                }
+        );
+    }
+
+    @Test
+    public void findByIdThatDoesNotExist() throws Exception {
+        Optional<Officer> officer = dao.findById(187);
+        assertThat(officer.isEmpty());
+    }
+
+    @Test
+    public void findAll() throws Exception {
+        List<String> officerNames = dao.findAll().stream()
+                .map(Officer::getLast)
+                .collect(Collectors.toList());
+        assertAll(
+                () -> assertThat(officerNames).isNotNull(),
+                () -> assertThat(officerNames.size()).isEqualTo(5),
+                () -> assertThat(officerNames).containsExactly("Kirk", "Picard", "Sisko", "Janeway", "Archer")
+        );
+    }
+
+    @Test
+    public void count() throws Exception {
+        assertThat(dao.count()).isEqualTo(5);
+    }
 
     @Test
     public void findOneThatExists() throws Exception {
